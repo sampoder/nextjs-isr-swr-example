d01a0349bb83be7453d8e6c2bcf01b5bbcd39b7a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-14 13:01:16 +0800

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,8 +1,4 @@
-export async function getTaxiData(){
-	let data = await fetch("https://api.data.gov.sg/v1/transport/taxi-availability").then(r => r.json())
-    console.log(data)
-	return {taxis: data.features[0].properties.taxi_count, updatedAt: data.features[0].properties.timestamp}
-}
+import { getTaxiData } from '../../lib/helpers'
 
 export default async function handler(req, res){
 	res.status(200).json(await getTaxiData())
