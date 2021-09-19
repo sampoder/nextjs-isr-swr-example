38528247b1506e54291a4ac0cfd2da2b5074f602
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-09-19 09:34:36 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import  useSWR from 'swr'
 
 export default function App(props){
     const fetcher = (...args) => fetch(...args).then(res => res.json())
-    const { data } = useSWR("/api", fetcher, {initialData: props, refreshInterval: 30000})
+    const { data } = useSWR("/api", fetcher, {fallbackData: props, refreshInterval: 30000})
     return <div>As of {data.updatedAt}, there are {data.taxis} taxis available in Singapore!</div>
 }
 
